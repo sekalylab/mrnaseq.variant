eb3fdb80913edd92fcd0e33d6b74156cf25942e7
Slim Fourati <sxf278@case.edu>
2018-06-05 12:19:54 -0400

    switching to gatk4.0 for steps after recalibration

--- mRNA.variant_seq.sh
+++ mRNA.variant_seq.sh
@@ -4,7 +4,8 @@
 # load apps
 module load STAR/2.5.3a
 module load picard/2.11
-module load gatk/3.8
+module load java/8u121
+module load gatk/4.0.1.1
 module load samtools/1.8
 
 # read arguments
@@ -26,7 +27,7 @@ maxProc=8
 genomeDir="$seqDependencies/ggNoOverhang"
 
 # Path to reference genome and Index files.
-flag=true
+flag=false
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
@@ -69,7 +70,7 @@ then
 fi
 
 # Order BAM file by position
-flag=true
+flag=false
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
@@ -89,7 +90,7 @@ then
 fi
 
 # Index BAM file
-flag=true
+flag=false
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
@@ -105,7 +106,7 @@ then
 fi
 
 # Mark duplicates using Picard
-flag=true
+flag=false
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
@@ -127,7 +128,7 @@ then
 fi
 
 # SplitNCigarReads
-flag=true
+flag=false
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
@@ -152,67 +153,21 @@ then
     echo -ne "done\n"
 fi
 
-# Create targets for indel realignment
-flag=true
-if $flag
-then
-    currentDate=$(date +"%Y-%m-%d %X")
-    echo -ne "$currentDate: creating targets for realignment..."
-    for sample in $(find $dirData -name "*.split.bam")
-    do
-	sampleID=$(echo $sample | sed -r 's/(.?).split.bam$/\1/')
-	java -d64 -jar $GATK \
-            -T RealignerTargetCreator \
-	    -R $genomeFasta \
-	    -I $sample \
-	    -o $sampleID.intervals \
-	    -nt $maxProc 2>$sampleID.indel.log
-	# -known $seqDependencies/Annotation/Mills_and_1000G_gold_standard.indels.hg38.vcf
-	# -known $seqDependencies/Annotation/Homo_sapiens_assembly38.known_indels.vcf
-    done
-    echo -ne "done\n"
-fi
-
-# Perform indel realignment
-flag=true
-if $flag
-then
-    currentDate=$(date +"%Y-%m-%d %X")
-    echo -ne "$currentDate: perform indel realignment..."
-    for sample in $(find $dirData -name "*.split.bam")
-    do
-        sampleID=$(echo $sample | sed -r 's/(.?).split.bam$/\1/')
-        java -d64 -Xmx30G -jar $GATK \
-            -T IndelRealigner \
-            -R $genomeFasta \
-            -targetIntervals $sampleID.intervals \
-	    -I $sample \
-            -o $sampleID.processed.bam 2>$sampleID.indel2.log
-            # -known $seqDependencies/Annotation/Mills_and_1000G_gold_standard.indels.hg38.vcf
-            # -known $seqDependencies/Annotation/Homo_sapiens_assembly38.known_indels.vcf
-	# remove files
-	rm $sample
-	rm $sampleID.split.bai
-	rm $sampleID.intervals
-    done
-    echo -ne "done\n"
-fi
-
 # Generate recalibration table
 flag=true
 if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
     echo -ne "$currentDate: generate recalibration table..."
-    for sample in $(find $dirData -name "*.processed.bam")
+    for sample in $(find $dirData -name "*.split.bam")
     do
-	sampleID=$(echo $sample | sed -r 's/(.?).processed.bam$/\1/')
+	sampleID=$(echo $sample | sed -r 's/(.?).split.bam$/\1/')
 	java -d64 -jar $GATK \
-	    -T BaseRecalibrator \
+	    BaseRecalibrator \
 	    -I $sample \
 	    -R $genomeFasta \
 	    -knownSites $seqDependencies/Annotation/variant.vcf \
-	    -o $sampleID.recal.table 2>$sampleID.genBaseRecalib.log
+	    -O $sampleID.recal.table 2>$sampleID.genBaseRecalib.log
     done
     echo -ne "done\n"
 fi
@@ -223,19 +178,18 @@ if $flag
 then
     currentDate=$(date +"%Y-%m-%d %X")
     echo -ne "$currentDate: printing recalibrated reads..."
-    for sample in $(find $dirData -name "*.processed.bam")
+    for sample in $(find $dirData -name "*.split.bam")
     do
-	sampleID=$(echo $sample | sed -r 's/(.?).processed.bam$/\1/')
-	java -d64 -Xmx30G -jar $GATK \
-	    -T PrintReads \
+	sampleID=$(echo $sample | sed -r 's/(.?).split.bam$/\1/')
+	java -d64 -Xmx32G -jar $GATK \
+	    ApplyBQSR \
 	    -R $genomeFasta \
 	    -I $sample \
-	    -nct $maxProc \
-	    -BQSR $sampleID.recal.table \
-	    -o $sampleID.recal.bam 2>$sampleID.baseRecalib.log
+	    --bqsr-recal-file $sampleID.recal.table \
+	    -O $sampleID.recal.bam 2>$sampleID.baseRecalib.log
 	# remove files
-	rm $sample
-	rm $sampleID.processed.bai
+	# rm $sample
+	# rm $sampleID.split.bai
     done
     echo -ne "done\n"
 fi
@@ -249,19 +203,15 @@ then
     for sample in $(find $dirData -name "*.recal.bam")
     do
 	sampleID=$(echo $sample | sed -r 's/(.?).recal.bam$/\1/')
-	java -d64 -Xmx30G \
+	java -d64 -Xmx32G \
 	    -jar $GATK \
-	    -T HaplotypeCaller \
+	    HaplotypeCaller \
 	    -R $genomeFasta \
 	    -I $sample \
-	    -dontUseSoftClippedBases \
-	    -stand_call_conf 20 \
+	    --dont-use-soft-clipped-bases \
+	    --standard-min-confidence-threshold-for-calling 20 \
 	    -ERC GVCF \
-	    -variant_index_type LINEAR \
-	    -variant_index_parameter 128000 \
-	    -nct $maxProc \
-	    -o $sampleID.vcf 2>$sampleID.haploCaller.log
-	# -stand_emit_conf 20 is obsolete
+	    -O $sampleID.vcf 2>$sampleID.haploCaller.log
 	# remove files
 	# rm $sample
 	# rm $sampleID.recal.bai
@@ -280,7 +230,7 @@ then
     do
 	sampleID=$(echo $sample | sed -r 's/(.?).vcf$/\1/')
 	java -d64 -jar $GATK \
-	    -T VariantFiltration \
+	    VariantFiltration \
 	    -R $genomeFasta \
 	    -V $sample \
 	    -window 35 \
@@ -289,7 +239,7 @@ then
 	    --filterExpression "FS > 30.0" \
 	    --filterName QD \
 	    --filterExpression "QD < 2.0" \
-	    -o $sampleID.filtered.vcf 2>$sampleID.variantFilter.log
+	    -O $sampleID.filtered.vcf 2>$sampleID.variantFilter.log
 	# remove files
 	# rm $sample
 	# rm $sampleID.vcf.idx
